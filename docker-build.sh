ba7b739baf7b265580a0a6ecb767fe96315bb49b
vicky.lim <minhow.lim@gmail.com>
2026-08-23 10:24:43 +0800

    chore: remove docker-build.sh from repo

--- docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Build and push multi-arch image
-# For private registry:
-#   docker buildx build --platform linux/amd64,linux/arm64 -t repo.vlmh.work/bentopdf:latest --push .
-# For Docker Hub (replace YOUR_DOCKERHUB_USER):
-#   docker buildx build --platform linux/amd64,linux/arm64 -t YOUR_DOCKERHUB_USER/bentopdf:latest --push .
-
-docker buildx build --platform linux/amd64,linux/arm64 -t repo.vlmh.work/bentopdf:latest --push .
